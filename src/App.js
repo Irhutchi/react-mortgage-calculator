7ed085a28c62821cddd36c33df5a8ce713a27d90
Ian <ianhutch90@gmail.com>
2023-10-01 12:48:58 +0100

    Add Mortgage Result Calculations

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
             <TenureSelect data={data} setData={setData} />
           </Grid>
           <Grid item xs={12} md={6}>
-            <Result />
+            <Result data={data} />
           </Grid>
         </Grid>
       </Container>
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Stack, Typography } from "@mui/material";
+import React from "react";
 
-const Result = () => {
+const Result = ({ data }) => {
+  const { propertyValue, downPayment, loanAmount, loanTerm, interestRate } =
+    data;
+
+  const totalLoanMonths = loanTerm * 12; // 120 months = 10 years
+  const interestPerMonth = interestRate / 100 / 12; // €100 @ 12% for 1 year
+
+  const monthyPayment =
+    (loanAmount *
+      interestPerMonth *
+      (1 + interestPerMonth) ** totalLoanMonths) /
+    ((1 * interestPerMonth) ** totalLoanMonths - 1);
+
+  const totalInterestGenerated = monthyPayment * totalLoanMonths - loanAmount;
+
   return (
-    <div>Result</div>
-  )
-}
+    <Stack>
+      <Typography>Monthly Payment: € {monthyPayment.toFixed(2)}</Typography>
+    </Stack>
+  );
+};
 
-export default Result
+export default Result;
